1a66b58022de41ffd25991437515e5b3e4e2872b
Mariusz Smenżyk <mariusz.smenzyk@sizeof.pl>
2016-05-19 00:48:36 +0200

    Dist

--- dist/angular-brave-auth.js
+++ dist/angular-brave-auth.js
@@ -11,6 +11,98 @@
 
 })();
 
+(function () {
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .controller('LoginController', LoginController);
+
+  LoginController.$inject = ['$location', '$scope', 'AuthService'];
+
+  /**
+   *
+   * @param {object} $location location
+   * @param {object} $scope scope
+   * @param {object} authService AuthService object
+   * @constructor
+     */
+  function LoginController($location, $scope, authService) {
+    var vm = this;
+
+    vm.login = login;
+
+    activate();
+
+    /**
+     * @name activate
+     * @desc Actions to be performed when this controller is instantiated
+     * @memberOf app.auth.LoginController
+     */
+    function activate() {
+      // If the user is authenticated, they should not be here.
+      // if (authService.isAuthenticated()) {
+      //    $location.url('/');
+      // }
+    }
+
+    /**
+     * @name login
+     * @desc Log the user in
+     * @memberOf app.auth.LoginController
+     */
+    function login() {
+      authService.login(vm.username, vm.password);
+    }
+
+    /**
+     $scope.$on('event:google-plus-signin-success', function (event, authResult) {
+      if (authResult.status.method == 'PROMPT') {
+        GooglePlus.getUser().then(function (user) {
+          User.username = user.name;
+          User.picture = user.picture;
+          $state.go('app.dashboard');
+        });
+      }
+    });
+
+    $scope.$on('event:facebook-signin-success', function (event, authResult) {
+      ezfb.api('/me', function (res) {
+        User.username = res.name;
+        User.picture = 'https://graph.facebook.com/' + res.id + '/picture';
+        $state.go('app.dashboard');
+      });
+    });*/
+  }
+})();
+
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name app [app.auth]
+   * @ Logout controller
+   */
+  angular
+    .module('app.auth')
+    .controller('LogoutController', LogoutController);
+
+  LogoutController.$inject = ['AuthService'];
+
+  /**
+   *
+   * @param {object} authService AuthService object
+   * @description Auth service
+   * @constructor
+   */
+  function LogoutController(authService) {
+    authService.logout();
+  }
+
+})();
+
+
 (function() {
   'use strict';
 
@@ -134,98 +226,6 @@
   }
 
 })();
-
-(function () {
-  'use strict';
-
-  angular
-    .module('app.auth')
-    .controller('LoginController', LoginController);
-
-  LoginController.$inject = ['$location', '$scope', 'AuthService'];
-
-  /**
-   *
-   * @param {object} $location location
-   * @param {object} $scope scope
-   * @param {object} authService AuthService object
-   * @constructor
-     */
-  function LoginController($location, $scope, authService) {
-    var vm = this;
-
-    vm.login = login;
-
-    activate();
-
-    /**
-     * @name activate
-     * @desc Actions to be performed when this controller is instantiated
-     * @memberOf app.auth.LoginController
-     */
-    function activate() {
-      // If the user is authenticated, they should not be here.
-      // if (authService.isAuthenticated()) {
-      //    $location.url('/');
-      // }
-    }
-
-    /**
-     * @name login
-     * @desc Log the user in
-     * @memberOf app.auth.LoginController
-     */
-    function login() {
-      authService.login(vm.username, vm.password);
-    }
-
-    /**
-     $scope.$on('event:google-plus-signin-success', function (event, authResult) {
-      if (authResult.status.method == 'PROMPT') {
-        GooglePlus.getUser().then(function (user) {
-          User.username = user.name;
-          User.picture = user.picture;
-          $state.go('app.dashboard');
-        });
-      }
-    });
-
-    $scope.$on('event:facebook-signin-success', function (event, authResult) {
-      ezfb.api('/me', function (res) {
-        User.username = res.name;
-        User.picture = 'https://graph.facebook.com/' + res.id + '/picture';
-        $state.go('app.dashboard');
-      });
-    });*/
-  }
-})();
-
-(function () {
-  'use strict';
-
-  /**
-   * @ngdoc overview
-   * @name app [app.auth]
-   * @ Logout controller
-   */
-  angular
-    .module('app.auth')
-    .controller('LogoutController', LogoutController);
-
-  LogoutController.$inject = ['AuthService'];
-
-  /**
-   *
-   * @param {object} authService AuthService object
-   * @description Auth service
-   * @constructor
-   */
-  function LogoutController(authService) {
-    authService.logout();
-  }
-
-})();
-
 
 (function () {
   'use strict';
