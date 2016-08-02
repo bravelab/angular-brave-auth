a3fd4359f5516c4675d26a84a2824102f0d654fd
Mariusz Smenżyk <mariusz.smenzyk@sizeof.pl>
2016-08-02 14:38:31 +0200

    Bump version

--- dist/angular-brave-auth.js
+++ dist/angular-brave-auth.js
@@ -16,81 +16,6 @@
 (function () {
   'use strict';
 
-  angular
-    .module('app.auth')
-    .controller('LoginController', LoginController);
-
-  LoginController.$inject = ['$location', '$stateParams', '$scope', 'AuthService', 'BraveAuthConfig'];
-  /**
-   *
-   * @param {object} $location location
-   * @param {object} $stateParams location
-   * @param {object} $scope scope
-   * @param {object} authService AuthService object
-   * @param {object} braveAuthConfig BraveAuthConfigProvider object
-   * @constructor
-     */
-  function LoginController($location, $stateParams, $scope, authService, braveAuthConfig) {
-
-    var vm = this;
-
-    vm.login = login;
-    vm.message = $stateParams.message; // TODO nkler: check if it works with $state.go($state.current ...)
-    vm.usernameFieldTemplate = braveAuthConfig.getUsernameFieldTemplate();
-    vm.logo = braveAuthConfig.getLogo();
-
-    activate();
-
-    /**
-     * @name activate
-     * @desc Actions to be performed when this controller is instantiated
-     * @memberOf app.auth.LoginController
-     */
-    function activate() {
-    }
-
-    /**
-     * @name login
-     * @desc Log the user in
-     * @memberOf app.auth.LoginController
-     */
-    function login() {
-      authService.login(vm.username, vm.password);
-    }
-  }
-
-})();
-
-(function () {
-  'use strict';
-
-  /**
-   * @ngdoc overview
-   * @name app [app.auth]
-   * @ Logout controller
-   */
-  angular
-    .module('app.auth')
-    .controller('LogoutController', LogoutController);
-
-  LogoutController.$inject = ['AuthService'];
-
-  /**
-   *
-   * @param {object} authService AuthService object
-   * @description Auth service
-   * @constructor
-   */
-  function LogoutController(authService) {
-    authService.logout();
-  }
-
-})();
-
-
-(function () {
-  'use strict';
-
   /**
    * @ngdoc overview
    * @name brave [app.auth]
@@ -239,6 +164,81 @@
   }
 
 })();
+
+(function () {
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .controller('LoginController', LoginController);
+
+  LoginController.$inject = ['$location', '$stateParams', '$scope', 'AuthService', 'BraveAuthConfig'];
+  /**
+   *
+   * @param {object} $location location
+   * @param {object} $stateParams location
+   * @param {object} $scope scope
+   * @param {object} authService AuthService object
+   * @param {object} braveAuthConfig BraveAuthConfigProvider object
+   * @constructor
+     */
+  function LoginController($location, $stateParams, $scope, authService, braveAuthConfig) {
+
+    var vm = this;
+
+    vm.login = login;
+    vm.message = $stateParams.message; // TODO nkler: check if it works with $state.go($state.current ...)
+    vm.usernameFieldTemplate = braveAuthConfig.getUsernameFieldTemplate();
+    vm.logo = braveAuthConfig.getLogo();
+
+    activate();
+
+    /**
+     * @name activate
+     * @desc Actions to be performed when this controller is instantiated
+     * @memberOf app.auth.LoginController
+     */
+    function activate() {
+    }
+
+    /**
+     * @name login
+     * @desc Log the user in
+     * @memberOf app.auth.LoginController
+     */
+    function login() {
+      authService.login(vm.username, vm.password);
+    }
+  }
+
+})();
+
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name app [app.auth]
+   * @ Logout controller
+   */
+  angular
+    .module('app.auth')
+    .controller('LogoutController', LogoutController);
+
+  LogoutController.$inject = ['AuthService'];
+
+  /**
+   *
+   * @param {object} authService AuthService object
+   * @description Auth service
+   * @constructor
+   */
+  function LogoutController(authService) {
+    authService.logout();
+  }
+
+})();
+
 
 (function() {
   'use strict';
