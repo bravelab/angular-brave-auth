cd54837efd593cc912ea435d6b54f2744e2b06de
Mariusz Smenżyk <mariusz.smenzyk@sizeof.pl>
2016-08-02 14:28:36 +0200

    Added dist

--- dist/angular-brave-auth.js
+++ dist/angular-brave-auth.js
@@ -7,10 +7,85 @@
    * @description Auth module of the application.
    */
   angular.module('app.auth', ['ui.router', 'ngCookies', 'ngStorage'])
-    .value('braveAuthVersion', '0.0.11');
+    .value('braveAuthVersion', '0.0.13');
 
 })();
 
+
+
+(function () {
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .controller('LoginController', LoginController);
+
+  LoginController.$inject = ['$location', '$stateParams', '$scope', 'AuthService', 'BraveAuthConfig'];
+  /**
+   *
+   * @param {object} $location location
+   * @param {object} $stateParams location
+   * @param {object} $scope scope
+   * @param {object} authService AuthService object
+   * @param {object} braveAuthConfig BraveAuthConfigProvider object
+   * @constructor
+     */
+  function LoginController($location, $stateParams, $scope, authService, braveAuthConfig) {
+
+    var vm = this;
+
+    vm.login = login;
+    vm.message = $stateParams.message; // TODO nkler: check if it works with $state.go($state.current ...)
+    vm.usernameFieldTemplate = braveAuthConfig.getUsernameFieldTemplate();
+    vm.logo = braveAuthConfig.getLogo();
+
+    activate();
+
+    /**
+     * @name activate
+     * @desc Actions to be performed when this controller is instantiated
+     * @memberOf app.auth.LoginController
+     */
+    function activate() {
+    }
+
+    /**
+     * @name login
+     * @desc Log the user in
+     * @memberOf app.auth.LoginController
+     */
+    function login() {
+      authService.login(vm.username, vm.password);
+    }
+  }
+
+})();
+
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name app [app.auth]
+   * @ Logout controller
+   */
+  angular
+    .module('app.auth')
+    .controller('LogoutController', LogoutController);
+
+  LogoutController.$inject = ['AuthService'];
+
+  /**
+   *
+   * @param {object} authService AuthService object
+   * @description Auth service
+   * @constructor
+   */
+  function LogoutController(authService) {
+    authService.logout();
+  }
+
+})();
 
 
 (function () {
@@ -164,82 +239,6 @@
   }
 
 })();
-
-(function () {
-  'use strict';
-
-  angular
-    .module('app.auth')
-    .controller('LoginController', LoginController);
-
-  LoginController.$inject = ['$location', '$stateParams', '$scope', 'AuthService', 'BraveAuthConfig'];
-  /**
-   *
-   * @param {object} $location location
-   * @param {object} $stateParams location
-   * @param {object} $scope scope
-   * @param {object} authService AuthService object
-   * @param {object} braveAuthConfig BraveAuthConfigProvider object
-   * @constructor
-     */
-  function LoginController($location, $stateParams, $scope, authService, braveAuthConfig) {
-
-    var vm = this;
-
-    vm.login = login;
-    vm.message = $stateParams.message; // TODO nkler: check if it works with $state.go($state.current ...)
-    vm.usernameFieldTemplate = braveAuthConfig.getUsernameFieldTemplate();
-    vm.logo = braveAuthConfig.getLogo();
-
-    activate();
-
-    /**
-     * @name activate
-     * @desc Actions to be performed when this controller is instantiated
-     * @memberOf app.auth.LoginController
-     */
-    function activate() {
-    }
-
-    /**
-     * @name login
-     * @desc Log the user in
-     * @memberOf app.auth.LoginController
-     */
-    function login() {
-      console.log('app.auth.LoginController');
-      authService.login(vm.username, vm.password);
-    }
-  }
-
-})();
-
-(function () {
-  'use strict';
-
-  /**
-   * @ngdoc overview
-   * @name app [app.auth]
-   * @ Logout controller
-   */
-  angular
-    .module('app.auth')
-    .controller('LogoutController', LogoutController);
-
-  LogoutController.$inject = ['AuthService'];
-
-  /**
-   *
-   * @param {object} authService AuthService object
-   * @description Auth service
-   * @constructor
-   */
-  function LogoutController(authService) {
-    authService.logout();
-  }
-
-})();
-
 
 (function() {
   'use strict';
--- test/angular-brave-auth.spec.js
+++ test/angular-brave-auth.spec.js
@@ -19,7 +19,7 @@
       });
 
       // it('should return current version', inject(function (braveAuthVersion) {
-      //  expect(braveAuthVersion).toEqual('0.0.13');
+      //  expect(braveAuthVersion).toEqual('0.0.14');
       // }));
 
     });
